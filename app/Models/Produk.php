62d309dab2d7b8a4cc74e8c0fb78ed9ebeb1127c
yeremia victor <yeremiavictor@gmail.com>
2025-04-16 14:53:55 +0700

    berikan akses untuk model

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+//import hasfactory
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Produk extends Model
 {
-    //
+    //panggil has factory
+    use HasFactory;
+
+    protected $fillable =[
+        'foto','nama','deskripsi','harga','stok'
+    ];
 }
