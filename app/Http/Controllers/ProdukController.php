a56a658f674ffdb00c59f2943295f8e95b4905f6
yeremia victor <yeremiavictor@gmail.com>
2025-04-16 15:06:22 +0700

    controller Produk -> View

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -4,7 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//import model
+use App\Models\Produk;
+
+//use view
+use Illuminate\View\View;
+
 class ProdukController extends Controller
 {
-    //
+    public function index(): View {
+        //mengambil seluruh produk
+        $produks = Produk::latest()->paginate(10);
+
+        //render view dengan produks
+        return view('produks.index', compact('produks'));
+    }
+
 }
