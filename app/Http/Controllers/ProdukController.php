f5939d693aa8ef79d0d07ad29239dba77761b297
yeremia victor <yeremiavictor@gmail.com>
2025-05-04 21:33:30 +0700

    insert create controller

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -10,6 +10,9 @@ use App\Models\Produk;
 //use view
 use Illuminate\View\View;
 
+//import untuk C.U.D
+use Illuminate\Http\RedirectResponse;
+
 class ProdukController extends Controller
 {
     public function index(): View {
@@ -19,5 +22,41 @@ class ProdukController extends Controller
         //render view dengan produks
         return view('produks.index', compact('produks'));
     }
+
+    //create
+
+    public function create(): View
+    {
+        return view('produks.create');
+    }
+
+    public function store(Request $request): RedirectResponse{
+        //form validasi
+        $request->validate([
+            'foto'      => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'nama'      => 'required|min:5',
+            'deskripsi' => 'required|min:10',
+            'harga'     => 'required|numeric',
+            'stok'      => 'required|numeric',
+
+        ]);
+
+        //upload foto
+        $foto=$request->file('foto');
+        $foto->storeAs('produk',$foto->hashName());
+
+        //buat produk
+        Produk::create([
+            'foto'      =>$foto->hashName(),
+            'nama'      =>$request->nama,
+            'deskripsi' =>$request->deskripsi,
+            'harga'     =>$request->harga,
+            'stok'      =>$request->stok,
+        ]);
+
+        //membuat redirect halaman ke index, setelah berhasil create
+        return redirect()->route('produk.index')->with(['success'=>'data tersimpan']);
+    }
+
 
 }
